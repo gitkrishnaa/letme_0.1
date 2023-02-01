976e21c83f9f3ea2bb36ead3a4616fd997d72d7c
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-02-01 22:28:52 +0530

    2 files modified

--- script.js
+++ script.js
@@ -19,7 +19,14 @@ div1.appendChild(div2);
 
 let div3=document.createElement("div");
 div1.appendChild(div3);
-for(let j=0;j<5;j++){
+let img1=document.createElement("IMG");
+div2.appendChild(img1);
+img1.setAttribute("src","https://raw.githubusercontent.com/gitkrishnaa/letme_0.1/main/hotel_img/17133d04c09fd8f8.avif")
+img1.setAttribute("width", "100%");
+img1.setAttribute("height", "100%");
+img1.setAttribute("alt", "The Pulpit Rock");
+
+for(let j=1;j<6;j++){
 let div3_1=document.createElement("div");
 div3.appendChild(div3_1);
 }
