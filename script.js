d13dbec49c2cf44913291d9c10b4071e03a97924
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-02-01 22:02:25 +0530

    2 files modified

--- script.js
+++ script.js
@@ -19,7 +19,10 @@ div1.appendChild(div2);
 
 let div3=document.createElement("div");
 div1.appendChild(div3);
-
+for(let j=0;j<5;j++){
+let div3_1=document.createElement("div");
+div3.appendChild(div3_1);
+}
 let div4=document.createElement("div");
 div1.appendChild(div4);
 
