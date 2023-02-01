54d510f344f377490ec7f4be9b732730528dff72
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-02-01 05:43:10 +0530

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('Hello!');
+console.log('Hello! ');
+fetch('https://gitkrishnaa.github.io/letme_0.1/data.json')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
