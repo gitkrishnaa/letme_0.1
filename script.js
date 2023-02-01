819754be4f0f3d4c6eb78656cdbb84531218f2f4
gitkrishnaa <84628880+gitkrishnaa@users.noreply.github.com>
2023-02-01 15:39:14 +0530

    3 files modified

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 console.log('Hello! ');
+
+let x =document.getElementById("main");
+console.log(x)
 fetch('./data.json')
   .then((response) => response.json())
-  .then((json) => console.log(json));
+  .then((json) =>{
+
+console.log(json)
+for(let i=0;i<json.length;i++){
+  console.log(json[i])
+let a1=document.createElement("div");
+x.appendChild(a1)
+
+
+
+
+
+
+
+
+
+
+}
+
+
+  } );
+
 
  
   
